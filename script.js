609cb674175e5005e898be84ad1327d22d525d5d
Monica <mdedomen1@gmail.com>
2021-09-02 22:06:36 -0400

    Complete

--- script.js
+++ script.js
@@ -1,34 +1,58 @@
 // Write your JavaScript code here!
 
-const { myFetch, pickPlanet, addDestinationInfo } = require("./scriptHelper");
+//TODO: blah
+
+//const { addDestinationInfo } = require("./scriptHelper");
 
 window.addEventListener("load", function() {
 
-   let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-  let listedPlanetsResponse = myFetch();
-
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
+    let listedPlanets;
+    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+    let listedPlanetsResponse = myFetch();
+    listedPlanetsResponse.then((listedPlanets) => {
+       //the array of planets is listedPlanets
+        let planet = pickPlanet(listedPlanets);
+        console.log("Adding destination info")
+        console.log(planet);
+        addDestinationInfo(
+            document, 
+            planet.name, 
+            planet.diameter, 
+            planet.star,
+            planet.distance,
+            planet.moons,
+            planet.image)
+   })
+        //.then(function () {
+        //console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
       //  const planet = pickPlanet(listedPlanets)
       //  addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl)
-   })
+
 
 
    // get the form
-   // add a listener to when the form submit
-  //  form.addEventListener('submit', function(event) {
-      // if it's not, preventDefault
-      // get the value for each of the input field
-      // let pilotInput = this.document.querySelector("input[name=pilotName]");
-      // const pilotValue = pilotInput.value
+   let form = document.querySelector("form");
+   //add a listener to when the form submit
+   form.addEventListener('submit', function(event) {
+      //if it's not, preventDefault
+      event.preventDefault();
+      //get the value for each of the input field
+      let pilotInput = document.querySelector("input[name=pilotName]");
+      const pilotValue = pilotInput.value;
 
-      // let list = document.getElementById('faultyItems');
-      // formSubmission(document, list, pilotValue, copilotValue, fuelLevelValue, cargoLevelValue)
+      let copilotInput = document.querySelector("input[name=copilotName]");
+      const copilotValue = copilotInput.value;
 
+      let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+      const fuelLevelValue = fuelLevelInput.value;
 
+      let cargoMassInput = document.querySelector("input[name=cargoMass]");
+      const cargoMassValue = cargoMassInput.value;
+
+      let list = document.getElementById('faultyItems');
+      formSubmission(document, list, pilotValue, copilotValue, fuelLevelValue, cargoMassValue)
+      addDestinationInfo(document, name, diameter, star, distance, moons, image);
+      //image or imageUrl, try both
+   });
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -1,42 +1,55 @@
 // Write your helper functions here!
-require('isomorphic-fetch');
+
+//UNCOMMENT BEFORE SUBMITTING
+//require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
   // get the missionTarget div
   // set the inner HTML to this
   // fill in the information that is passed in
    // Here is the HTML formatting for our mission target div.
-   /*
-    `
+   
+
+   //From studio 9/2/21
+   const div = document.getElementById("missionTarget");
+   div.innerHTML =
+
+                `
                 <h2>Mission Destination</h2>
                 <ol>
                     <li>Name: ${name}</li>
-                    <li>Diameter: </li>
+                    <li>Diameter: ${diameter}</li>
                     <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
+                    <li>Distance from Earth: ${distance}</li>
+                    <li>Number of Moons: ${moons}</li>
                 </ol>
-                <img src="">
+                <img src="${imageUrl}">
                 `
-   */
 }
 
 function validateInput(testInput) {
-  //check if the test Input is empty
-    // if it is, return 'Empty'
-  // check if it's not a number isNaN
-    // return 'Not a Number'
-  // else
-    // return 'Is a Number'
+  if (testInput === "") {
+    return "Empty"
+  } else if (isNaN(testInput)) {
+    return "Not a Number"
+  } else {
+    return "Is a Number"
+  }
 }
 
-function formSubmission(document, list, pilotValue, copilotValue, fuelLevelValue, cargoLevelValue) {
-  // check if any of the values are empty
+function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+
+// check if any of the values are empty
     // if (validateInput(pilotValue) === 'Empty' || validateInput(copilotValue) === 'Empty')
     // alert user that they need to fill out all the fields alert('message')
   // check if fuelLevelValue and cargoLevelValue are not numbers
     // alert the user that must enter valid input
 
+  if (validateInput(pilot) === "Empty" || validateInput(copilot) === "Empty" || validateInput(fuelLevel) === "Empty" || validateInput(cargoLevel) === "Empty") {
+    alert("All fields are required.  Please fill in all boxes.");
+  } else if (validateInput(fuelLevel) === "Not a Number" || validateInput(cargoLevel) === "Not a Number") {
+    alert("Please enter valid input.");
+  }
 
   // set the list.style.visibility = 'visible'
   // get the pilot status, update the inner HTML to say `Pilot ${pilotValue} is ready for launch`
@@ -51,14 +64,44 @@ function formSubmission(document, list, pilotValue, copilotValue, fuelLevelValue
 
     // if both fuel and cargo are good
       // change the launchStatus to "Shuttle is Ready for Launch" and color to green
+
+  let launchStatus = document.getElementById("launchStatus");
+  let fuelStatus = document.getElementById("fuelStatus");
+  let cargoStatus = document.getElementById("cargoStatus");
+  list.style.visibility = "visible";
+
+  pilot.innerHTML = `Pilot ${pilot} is ready for launch`;
+  copilot.innerHTML = `Co-Pilot ${copilot} is ready for launch`;
+
+  if (fuelLevel < 10000) {
+    launchStatus.innerHTML = "Shuttle not ready for launch";
+    launchStatus.style.color = "red";
+    fuelStatus.innerHTML = "Fuel level too low for launch";
+  }
+
+  if (cargoLevel > 10000) {
+    launchStatus.innerHTML = "Shuttle not ready for launch";
+    launchStatus.style.color = "red";
+    cargoStatus.innerHTML = "Cargo level too high for launch";
+  }
+
+  if (fuelLevel > 10000 && cargoLevel < 10000) {
+    launchStatus.innerHTML = "Shuttle is ready for launch";
+    launchStatus.style.color = "green";
+  }
 }
+
+
+
 
 async function myFetch() {
     let planetsReturned;
 
   planetsReturned = await fetch('https://handlers.education.launchcode.org/static/planets.json').then(function (response) {
-      // get the json from the response
-        });
+//get the json from the response
+      return response.json();
+
+      });
 
     return planetsReturned;
 }
@@ -66,6 +109,10 @@ async function myFetch() {
 function pickPlanet(planets) {
   // randomly pick a planet from the array
   // Math random for index
+
+  //From Studio 9/2/21
+  planet = planets[Math.floor(Math.random() * planets.length)];
+  return planet;
 }
 
 module.exports.addDestinationInfo = addDestinationInfo;
@@ -73,3 +120,13 @@ module.exports.validateInput = validateInput;
 module.exports.formSubmission = formSubmission;
 module.exports.pickPlanet = pickPlanet;
 module.exports.myFetch = myFetch;
+
+/*
+let promise - fetch("https://google.com/json")
+.then is a method that takes a function that it is going to run when it is done catching the thing, it will call that function and pass it a response
+so now if we do:
+console.log(response.data)
+
+//this is a much faster way to do it
+let promise = await fetch("https://google.com/json");
+*/
