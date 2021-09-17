b38196082997590f4bbd943f44814af49b556ecd
Monica <85039987+mdedomen4@users.noreply.github.com>
2021-09-16 21:18:27 -0400

    deleted comments

--- scriptHelper.js
+++ scriptHelper.js
@@ -1,16 +1,5 @@
-// Write your helper functions here!
-
-//UNCOMMENT BEFORE SUBMITTING
-//require('isomorphic-fetch');
-
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-  // get the missionTarget div
-  // set the inner HTML to this
-  // fill in the information that is passed in
-   // Here is the HTML formatting for our mission target div.
-   
 
-   //From studio 9/2/21
    const div = document.getElementById("missionTarget");
    div.innerHTML =
 
@@ -39,31 +28,11 @@ function validateInput(testInput) {
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
 
-// check if any of the values are empty
-    // if (validateInput(pilotValue) === 'Empty' || validateInput(copilotValue) === 'Empty')
-    // alert user that they need to fill out all the fields alert('message')
-  // check if fuelLevelValue and cargoLevelValue are not numbers
-    // alert the user that must enter valid input
-
   if (validateInput(pilot) === "Empty" || validateInput(copilot) === "Empty" || validateInput(fuelLevel) === "Empty" || validateInput(cargoLevel) === "Empty") {
     alert("All fields are required.  Please fill in all boxes.");
   } else if (validateInput(fuelLevel) === "Not a Number" || validateInput(cargoLevel) === "Not a Number") {
     alert("Please enter valid input.");
   }
-
-  // set the list.style.visibility = 'visible'
-  // get the pilot status, update the inner HTML to say `Pilot ${pilotValue} is ready for launch`
-  // get the copilot status, update the inner HTML to say `CoPilot ${copilotValue} is ready for launch`
-  // check if the fuel level is less 10,000
-    // change launchStatus to "Shuttle not ready for launch", and color to red
-    // change the fuelStatus to "Fuel level too low for launch"
-
-  // check if the cargo level is more than 10,000
-    // change launchStatus to "Shuttle not ready for launch", and color to red
-    // change the cargoStatus to "Cargo level too high for launch"
-
-    // if both fuel and cargo are good
-      // change the launchStatus to "Shuttle is Ready for Launch" and color to green
 
   let launchStatus = document.getElementById("launchStatus");
   let fuelStatus = document.getElementById("fuelStatus");
@@ -98,7 +67,6 @@ async function myFetch() {
     let planetsReturned;
 
   planetsReturned = await fetch('https://handlers.education.launchcode.org/static/planets.json').then(function (response) {
-//get the json from the response
       return response.json();
 
       });
@@ -107,10 +75,6 @@ async function myFetch() {
 }
 
 function pickPlanet(planets) {
-  // randomly pick a planet from the array
-  // Math random for index
-
-  //From Studio 9/2/21
   planet = planets[Math.floor(Math.random() * planets.length)];
   return planet;
 }
@@ -120,13 +84,3 @@ module.exports.validateInput = validateInput;
 module.exports.formSubmission = formSubmission;
 module.exports.pickPlanet = pickPlanet;
 module.exports.myFetch = myFetch;
-
-/*
-let promise - fetch("https://google.com/json")
-.then is a method that takes a function that it is going to run when it is done catching the thing, it will call that function and pass it a response
-so now if we do:
-console.log(response.data)
-
-//this is a much faster way to do it
-let promise = await fetch("https://google.com/json");
-*/
