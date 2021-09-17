ef8ae6fbfa69e38858a206cd5c92e0bc58b03a42
Monica <85039987+mdedomen4@users.noreply.github.com>
2021-09-16 21:14:07 -0400

    deleted comments

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-// Write your JavaScript code here!
-
-//TODO: blah
-
-//const { addDestinationInfo } = require("./scriptHelper");
-
 window.addEventListener("load", function() {
 
     let listedPlanets;
-    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+
     let listedPlanetsResponse = myFetch();
     listedPlanetsResponse.then((listedPlanets) => {
-       //the array of planets is listedPlanets
+
         let planet = pickPlanet(listedPlanets);
         console.log("Adding destination info")
         console.log(planet);
@@ -23,21 +17,15 @@ window.addEventListener("load", function() {
             planet.moons,
             planet.image)
    })
-        //.then(function () {
-        //console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-      //  const planet = pickPlanet(listedPlanets)
-      //  addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl)
 
 
 
-   // get the form
    let form = document.querySelector("form");
-   //add a listener to when the form submit
+
    form.addEventListener('submit', function(event) {
-      //if it's not, preventDefault
+
       event.preventDefault();
-      //get the value for each of the input field
+
       let pilotInput = document.querySelector("input[name=pilotName]");
       const pilotValue = pilotInput.value;
 
@@ -53,6 +41,6 @@ window.addEventListener("load", function() {
       let list = document.getElementById('faultyItems');
       formSubmission(document, list, pilotValue, copilotValue, fuelLevelValue, cargoMassValue)
       addDestinationInfo(document, name, diameter, star, distance, moons, image);
-      //image or imageUrl, try both
+
    });
 });
